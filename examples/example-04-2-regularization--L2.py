c15ad57aa55aaac85965b7603bb7a664f80d3bca
Alex Vasiliev <vasiliev.informatik@t-online.de>
2022-12-05 20:15:49 +0100

    update

--- examples/example-04-2-regularization--L2.py
+++ examples/example-04-2-regularization--L2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
 -- Regularization
+---- L2 Regularization
 Issue: Boston area housing and house pricing
 """
 # general libraries
@@ -84,7 +85,8 @@ print(dev_y.shape)
 # (107,)
 
 
-def create_and_train_model(data_train_norm, labels_train, data_dev_norm, labels_dev, num_neurons, num_layers):
+def create_and_train_l2_model(data_train_norm, labels_train, data_dev_norm, labels_dev,
+                              num_neurons, num_layers, num_epochs, reg_lambda):
     """
     This function builds and trains a feed-forward neural network model and evaluates it on the training and dev sets.
     """
@@ -92,10 +94,17 @@ def create_and_train_model(data_train_norm, labels_train, data_dev_norm, labels_
     inputs = keras.Input(shape=data_train_norm.shape[1])  # input layer
     # He initialization
     initializer = tf.keras.initializers.HeNormal()
+    # L2 regularization
+    reg = tf.keras.regularizers.l2(l2=reg_lambda)
     layer = inputs
     # customized number of layers and neurons per layer
     for i in range(num_layers):
-        layer = layers.Dense(num_neurons, activation='relu', kernel_initializer=initializer)(layer)  # hidden layers
+        layer = layers.Dense(
+            num_neurons,
+            activation='relu',
+            kernel_initializer=initializer,
+            kernel_regularizer=reg
+        )(layer)  # hidden layers
     # output layer: one neuron with the identity activation function
     outputs = layers.Dense(1)(layer)
     keras_model = keras.Model(inputs=inputs, outputs=outputs, name='model')
@@ -105,25 +114,37 @@ def create_and_train_model(data_train_norm, labels_train, data_dev_norm, labels_
     # Train model
     result = keras_model.fit(
         data_train_norm, labels_train,
-        epochs=10000, verbose=0,
+        epochs=num_epochs, verbose=0,
         batch_size=data_train_norm.shape[0],
         validation_data=(data_dev_norm, labels_dev),
         callbacks=[tfdocs.modeling.EpochDots()]
     )
     history = pd.DataFrame(result.history)
     history['epoch'] = result.epoch
+    # print performances
+    print('Cost function at epoch of 0:')
+    print(f"Training MSE = {history['loss'].values[0]}")
+    print(f"Dev MSE = {history['val_loss'].values[0]}")
+    print(f'Cost function at epoch of {num_epochs}:')
+    print(f"Training MSE = {history['loss'].values[-1]}")
+    print(f"Dev MSE = {history['val_loss'].values[-1]}")
     return history, keras_model
 
 
 num_l, num_n = 4, 20
-# hist, model = create_and_train_model(train_x, train_y, dev_x, dev_y, num_neurons=num_n, num_layers=num_l)
-# print(type(hist))  # <class 'pandas.core.frame.DataFrame'>
-# print(type(model))  # <class 'keras.engine.functional.Functional'>
-# hist.to_csv(f"../histories/history-04-1-num_n-{num_n}-num_l-{num_l}.csv")
-# model.save(f"../models/model-04-1-num_n-{num_n}-num_l-{num_l}")
+reg_lambdas = [0.0, 10.0]
+# for reg_lambda in reg_lambdas:
+#     hist, model = create_and_train_l2_model(
+#         train_x, train_y, dev_x, dev_y,
+#         num_neurons=num_n, num_layers=num_l, num_epochs=10000, reg_lambda=reg_lambda
+#     )
+#     hist.to_csv(f"../histories/history-04-2-num_n-{num_n}-num_l-{num_l}-L2_reg-{reg_lambda}.csv")
+#     model.save(f"../models/model-04-2-num_n-{num_n}-num_l-{num_l}-L2_reg-{reg_lambda}")
 
-hist = pd.read_csv(f"../histories/history-04-1-num_n-{num_n}-num_l-{num_l}.csv")
-model = tf.keras.models.load_model(f"../models/model-04-1-num_n-{num_n}-num_l-{num_l}")
+
+hist = pd.read_csv(f"../histories/history-04-2-num_n-{num_n}-num_l-{num_l}-L2_reg-10.0.csv")
+model_reg = tf.keras.models.load_model(f"../models/model-04-2-num_n-{num_n}-num_l-{num_l}-L2_reg-10.0")
+model_not_reg = tf.keras.models.load_model(f"../models/model-04-2-num_n-{num_n}-num_l-{num_l}-L2_reg-0.0")
 
 fp = set_style().set_general_style_parameters()
 plt.figure()
@@ -131,15 +152,15 @@ plt.plot(hist['loss'], ls='-', color='black', lw=3, label='Training MSE')
 plt.plot(hist['val_loss'], ls='--', color='blue', lw=2, label='Dev MSE')
 plt.ylabel('Cost Function (MSE)', fontproperties=fm.FontProperties(fname=fp))
 plt.xlabel('Number of Iterations', fontproperties=fm.FontProperties(fname=fp))
-plt.ylim(0, 30)
+plt.ylim(0, 200)
 plt.legend(loc='best')
 plt.axis(True)
 # plt.show()
-plt.savefig('../figures/figure-04-1-1.svg', bbox_inches='tight')
+plt.savefig('../figures/figure-04-2-1.svg', bbox_inches='tight')
 
 # predictions
-pred_y_train = model.predict(train_x).flatten()
-pred_y_dev = model.predict(dev_x).flatten()
+pred_y_train = model_reg.predict(train_x).flatten()
+pred_y_dev = model_reg.predict(dev_x).flatten()
 
 fig = plt.figure(figsize=(13, 5))
 ax = fig.add_subplot(121)
@@ -158,4 +179,74 @@ ax.set_ylim(0, 55)
 ax.legend(loc='best')
 plt.axis(True)
 # plt.show()
-plt.savefig('../figures/figure-04-1-2.svg', bbox_inches='tight')
+plt.savefig('../figures/figure-04-2-2.svg', bbox_inches='tight')
+
+# weights of non-regularized network
+weights1_not_reg = model_not_reg.layers[1].get_weights()
+weights2_not_reg = model_not_reg.layers[2].get_weights()
+weights3_not_reg = model_not_reg.layers[3].get_weights()
+weights4_not_reg = model_not_reg.layers[4].get_weights()
+
+print(weights1_not_reg[0].shape)  # 13 features to each of 20 neurons
+# (13, 20)
+print(weights1_not_reg[1].shape)  # 1 bias to each of 20 neurons
+# (20,)
+
+print(weights2_not_reg[0].shape)  # 20 inputs to each of 20 neurons
+# (20, 20)
+print(weights2_not_reg[1].shape)  # 1 bias to each of 20 neurons
+# (20,)
+
+print(weights3_not_reg[0].shape)  # 20 inputs to each of 20 neurons
+# (20, 20)
+print(weights3_not_reg[1].shape)  # 1 bias to each of 20 neurons
+# (20,)
+
+print(weights4_not_reg[0].shape)  # 20 inputs to each of 20 neurons
+# (20, 20)
+print(weights4_not_reg[1].shape)  # 1 bias to each of 20 neurons
+# (20,)
+
+# weights of regularized network
+weights1_reg = model_reg.layers[1].get_weights()
+weights2_reg = model_reg.layers[2].get_weights()
+weights3_reg = model_reg.layers[3].get_weights()
+weights4_reg = model_reg.layers[4].get_weights()
+
+fig = plt.figure(figsize=(12, 8))
+ax = fig.add_subplot(221)
+plt.hist(weights1_not_reg[0].flatten(), alpha=0.25, bins=10, color='black')
+plt.hist(weights1_reg[0].flatten(), alpha=0.5, bins=5, color='black')
+ax.set_ylabel('Count', fontproperties=fm.FontProperties(fname=fp))
+ax.text(-1, 150, 'Layer 1', fontproperties=fm.FontProperties(fname=fp))
+plt.xticks(fontproperties= fm.FontProperties(fname=fp))
+plt.yticks(fontproperties=fm.FontProperties(fname=fp))
+plt.ylim(0, 350)
+
+ax = fig.add_subplot(222)
+plt.hist(weights2_not_reg[0].flatten(), alpha=0.25, bins=10, color='black')
+plt.hist(weights2_reg[0].flatten(), alpha=0.5, bins=5, color='black')
+ax.text(-1.25, 150, 'Layer 2', fontproperties=fm.FontProperties(fname=fp))
+plt.xticks(fontproperties=fm.FontProperties(fname=fp))
+plt.yticks(fontproperties=fm.FontProperties(fname=fp))
+plt.ylim(0, 350)
+
+ax = fig.add_subplot(223)
+plt.hist(weights3_not_reg[0].flatten(), alpha=0.25, bins=10, color='black')
+plt.hist(weights3_reg[0].flatten(), alpha=0.5, bins=5, color='black')
+ax.set_ylabel('Count', fontproperties=fm.FontProperties(fname=fp))
+ax.set_xlabel('Weights', fontproperties=fm.FontProperties(fname=fp))
+ax.text(-2.30, 150, 'Layer 3', fontproperties=fm.FontProperties(fname=fp))
+plt.xticks(fontproperties=fm.FontProperties(fname=fp))
+plt.yticks(fontproperties=fm.FontProperties(fname=fp))
+plt.ylim(0, 400)
+
+ax = fig.add_subplot(224)
+plt.hist(weights4_not_reg[0].flatten(), alpha=0.25, bins=10, color='black')
+plt.hist(weights4_reg[0].flatten(), alpha=0.5, bins=5, color='black')
+ax.set_xlabel('Weights', fontproperties=fm.FontProperties(fname=fp))
+ax.text(-2.30, 150, 'Layer 4', fontproperties=fm.FontProperties(fname=fp))
+plt.xticks(fontproperties=fm.FontProperties(fname=fp))
+plt.yticks(fontproperties=fm.FontProperties(fname=fp))
+plt.ylim(0, 400)
+plt.savefig('../figures/figure-04-2-3.svg', bbox_inches='tight')
