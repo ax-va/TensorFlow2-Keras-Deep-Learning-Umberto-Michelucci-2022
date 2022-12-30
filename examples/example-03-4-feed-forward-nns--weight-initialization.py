72551574d8aa99e3e69ce882bfdc687ec52349c1
Alex Vasiliev <vasiliev.informatik@t-online.de>
2022-12-30 13:45:01 +0100

    update

--- examples/example-03-4-feed-forward-nns--weight-initialization.py
+++ examples/example-03-4-feed-forward-nns--weight-initialization.py
@@ -25,7 +25,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
-import time
 
 # tensorflow libraries
 from keras.datasets import fashion_mnist
