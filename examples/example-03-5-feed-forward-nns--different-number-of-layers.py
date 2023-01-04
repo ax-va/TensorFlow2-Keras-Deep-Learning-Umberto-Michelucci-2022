ea21097b5f71948c6fb65823921f0583f8d8ce82
Alex Vasiliev <vasiliev.informatik@t-online.de>
2023-01-05 00:42:28 +0100

    update

--- examples/example-03-5-feed-forward-nns--different-number-of-layers.py
+++ examples/example-03-5-feed-forward-nns--different-number-of-layers.py
@@ -12,6 +12,14 @@ gray value of the image (you can calculate that 28x28=784).
 {0: "T-shirt/top", 1: "Trouser", 2: "Pullover", 3: "Dress", 4: "Coat",
 5: "Sandal", 6: "Shirt", 7: "Sneaker", 8: "Bag", 9: "Ankle boot"}
 """
+import pathlib
+import sys
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
 # general libraries
 import pandas as pd
 import numpy as np
@@ -19,15 +27,14 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
 # tensorflow libraries
-from tensorflow import keras
 from keras.datasets import fashion_mnist
-from keras import layers
 import tensorflow as tf
-import tensorflow_docs as tfdocs
-import tensorflow_docs.modeling
 
 import importlib
 set_style = importlib.import_module("ADL-Book-2nd-Ed.modules.style_setting").set_style
+
+# my module
+import utils.feed_forward as feed_forward
 
 # train_x, test_x contain pixels.
 # train_y, test_y contain digits corresponding to Zalando clothes.
@@ -60,62 +67,95 @@ labels_train[np.arange(60000), train_y] = 1
 labels_test = np.zeros((10000, 10))
 labels_test[np.arange(10000), test_y] = 1
 
+# The last number is the number of outputs
+structures = ["10-10", "10-10-10", "10-10-10-10", "10-10-10-10-10", "100-100-100-100-10"]
+EPOCHS = 1000
+MB_SIZE = 20
 
-def build_and_train_model(num_neurons, num_layers):
-    """ Build and train model """
-    # Build model
-    inputs = keras.Input(shape=784)  # input layer
-    # customized number of layers and neurons per layer
-    layer = inputs
-    for i in range(num_layers):
-        layer = layers.Dense(num_neurons, activation='relu')(layer)  # hidden layers
-    # output layer
-    outputs = layers.Dense(10, activation='softmax')(layer)
-    model = keras.Model(inputs=inputs, outputs=outputs, name='model')
-    # Set optimizer and loss
-    model.compile(
-        loss='categorical_crossentropy',
+for structure in structures:
+    print("*" * 65)
+    print(f"Structure: {structure}")
+    model = feed_forward.build_keras_model(
+        num_inputs=784,
+        structure=structure,
+        hidden_activation="relu",
+        output_activation="softmax",
         optimizer=tf.keras.optimizers.SGD(momentum=0.9, learning_rate=0.0001),
-        metrics=['categorical_accuracy']
+        loss="categorical_crossentropy",
+        metrics=["categorical_accuracy"]
     )
-    # Train model
-    result = model.fit(
-        data_train_norm, labels_train,
-        epochs=1000, verbose=0,
-        batch_size=20,
-        callbacks=[tfdocs.modeling.EpochDots()]
+
+    learning_history, learning_time = feed_forward.fit_model(
+        model, data_train_norm, labels_train,
+        batch_size=MB_SIZE, num_epochs=EPOCHS
     )
-    history = pd.DataFrame(result.history)
-    history['epoch'] = result.epoch
-    return history, model
 
+    learning_history.to_parquet(f"../histories/history-03-5-mb_size-{MB_SIZE}-structure-{structure}.parquet")
+    # Save the trained model
+    model.save(f"../models/model-03-5-structure-{structure}")
+# *****************************************************************
+# Structure: 10-10
+# ...
+# Cost function at epoch of 0:
+# Training MSE = 1.4168392419815063
+# Cost function at epoch of 1000:
+# Training MSE = 0.3320516049861908
+# Learning time = 43.77 minutes
+# *****************************************************************
+# Structure: 10-10-10
+# ...
+# Cost function at epoch of 0:
+# Training MSE = 1.8717634677886963
+# Cost function at epoch of 1000:
+# Training MSE = 0.2803069055080414
+# Learning time = 46.32 minutes
+# *****************************************************************
+# Structure: 10-10-10-10
+# ...
+# Cost function at epoch of 0:
+# Training MSE = 2.0893638134002686
+# Cost function at epoch of 1000:
+# Training MSE = 0.28231358528137207
+# Learning time = 48.00 minutes
+# *****************************************************************
+# Structure: 10-10-10-10-10
+# ...
+# Cost function at epoch of 0:
+# Training MSE = 2.0132534503936768
+# Cost function at epoch of 1000:
+# Training MSE = 0.2618364095687866
+# Learning time = 49.71 minutes
+# *****************************************************************
+# Structure: 100-100-100-100-100-10
+# ...
+# Cost function at epoch of 0:
+# Training MSE = 1.7035552263259888
+# Cost function at epoch of 1000:
+# Training MSE = 0.00025658542290329933
+# Learning time = 73.97 minutes
 
-# history_dict = {}
-# for num_n, num_l in [(10, 1), (10, 2), (10, 3), (10, 4), (100, 4)]:
-#     df_history, model = build_and_train_model(num_neurons=num_n, num_layers=num_l)
-#     history_dict[(num_n, num_l)] = df_history
-#     df_history.to_csv(f"../histories/history-03-5-num_n-{num_n}-num_l-{num_l}.csv")
-
-
-history_dict = {}
-for num_n, num_l in [(10, 1), (10, 2), (10, 3), (10, 4), (100, 4)]:
-    df_history = pd.read_csv(f"../histories/history-03-5-num_n-{num_n}-num_l-{num_l}.csv")
-    history_dict[(num_n, num_l)] = df_history
-
+learning_history_dict = {}
+for structure in structures:
+    learning_history = pd.read_parquet(f"../histories/history-03-5-mb_size-20-structure-{structure}.parquet")
+    learning_history_dict[structure] = learning_history
 
 fp = set_style().set_general_style_parameters()
 fig = plt.figure()
-for params, color in [((10, 1), "black"), ((10, 2), "blue"), ((10, 3), "red"), ((10, 4), "green"), ((100, 4), "magenta")]:
-    df_history = history_dict[params]
-    if params[1] == 1:
-        text_num_layers = f'{params[1]} layer'
-        text_num_neurons = f'{params[0]} neurons'
+for structure in structures:
+    learning_history = learning_history_dict[structure]
+    neurons_per_layer = [int(num_str) for num_str in structure.split("-")]
+    num_layers = len(neurons_per_layer)
+    num_hidden_layers = num_layers - 1
+    if num_hidden_layers == 1:
+        text_num_layers = f'{num_hidden_layers} hidden layer'
+        text_num_neurons = f'{neurons_per_layer[0]} neurons'
     else:
-        text_num_layers = f'{params[1]} layers'
-        text_num_neurons = f'{params[0]} neurons per layer'
-    plt.plot(df_history['epoch'], df_history['loss'], color=color, label=f'{text_num_layers}, {text_num_neurons}')
+        text_num_layers = f'{num_hidden_layers} hidden layers'
+        text_num_neurons = f'{neurons_per_layer[0]} neurons per layer'
+    plt.plot(learning_history['epoch'], learning_history['loss'], label=f'{text_num_layers}, {text_num_neurons}')
 plt.ylabel('Cost function $J$', fontproperties=fm.FontProperties(fname=fp))
 plt.xlabel('Epochs', fontproperties=fm.FontProperties(fname=fp))
+plt.title('Different number of layers', fontproperties=fm.FontProperties(fname=fp))
 plt.legend(loc='best')
 plt.ylim(0.0, 1.0)
 plt.xlim(0, 1000)
